83e6ac675d444b154d6e708f4cc444c95a34c338
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-02-10 21:43:16 +0100

    requesting API

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+
+const Card = memo((props) => {
+  return (
+    <div>Card</div>
+  )
+})
+
+Card.propTypes = {}
+
+export default Card
